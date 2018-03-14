f762b25bebf2bd1e041eb0021b34fda6e522dc32
sluscott <steve.luscott@gmail.com>
2018-03-14 21:33:03 +0000

    Refactored to fix event handling context. Fixes bug in string manipulation where string 'tabs' is used in custom classes. Adds active classes to first elements. Updates jQuery to version 2.2.4 in demo

--- jquery.tabacco.js
+++ jquery.tabacco.js
@@ -11,42 +11,45 @@
  */
 (function($, undefined) {
     $.fn.tabacco = function tabRoot(options) {
+        var $context = this;
         var conf = {};
         $.fn.tabacco.settings = {
-            $tabs: $(".tabacco-tabs li a"),
-            $panels: $(".tabacco-panel")
+            $tabs: $(".tabacco-tabs li a", this),
+            $panels: $(".tabacco-panel", this)
         }
         $.extend(conf, this.tabacco.settings, options);
         function stripPrefix(a) {
             return a.slice(1, a.length);
         }
-        function tabHandler(elem) {
-            var targetNo = elem[0].tabIndex;
-            var elemSel = conf.$panels.selector;
-            var stripdSelector = stripPrefix(elemSel);
-            var $showPanel = $("#" + stripdSelector + "-" + targetNo);
-            conf.$panels.hide().removeClass("active");
+        function tabHandler($elem) {
+            var str = $elem[0].id;
+            var strSliceIndx = str.lastIndexOf("tab");
+            var str1 = str.slice(0,strSliceIndx);
+            var str2 = str.slice(strSliceIndx,str.length);
+            var str3 = str2.replace("tab","panel");
+            var str4 = str1 + str3;
+            var $targetPanel = $("#" + str4);
+            var $hidePanels = ($(conf.$panels.selector));
+            $hidePanels.hide().removeClass("active");
             conf.$tabs.removeClass("active");
-            elem.addClass("active");
-            $showPanel.toggleClass("active").show();
-        }
-        function setBindingIds(elem, index, type) {
-            if (type === true) {
-                elem.attr("id", stripPrefix(conf.$tabs.selector) + "-" + index);
-            } else {
-                elem.attr("id", stripPrefix(conf.$panels.selector) + "-" + index);
-            }
-            elem.attr("tabindex", index);
+            $elem.addClass("active");
+            $targetPanel.toggleClass("active").show();
         }
         conf.$tabs.each(function setTabs(index) {
-            var isTab = true;
-            setBindingIds($(this), index, isTab);
+            $(this).attr("id", stripPrefix($context.selector) + "-" + "tabacco-tab" + "-" + index);
+            $(this).attr("tabindex", index);
+            if(index == 0) {
+                $(this).addClass("active");
+            }
         });
         conf.$panels.each(function setPanels(index) {
-            var isTab = false;
-            setBindingIds($(this), index, isTab);
+            $(this).attr("id", stripPrefix($context.selector) + "-" + "tabacco-panel" + "-" + index);
+            $(this).attr("tabindex", index);
             if(index !== 0) {
                 $(this).hide();
+            }
+            if(index == 0) {
+                $(this).addClass("active");
             }
         });
         conf.$tabs.click(function(e) {
